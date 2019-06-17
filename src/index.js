5ed0fd9e99ca812030b05d92865b6a3e8ee5ea48
luis barroso <luis.barroso@ifood.com.br>
2019-06-17 20:56:24 -0300

    dale

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "./modules/Home";
 import React from "react";
+import { render } from "react-dom";
 
-export default (App = () => {
-  (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path={"/"} component={Home} />
-      </Switch>
-    </BrowserRouter>
-  ),
-    document.getElementById("root");
-});
+render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path={"/"} component={Home} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);

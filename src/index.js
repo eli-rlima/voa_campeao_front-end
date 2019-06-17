68f9a3b17c92bc7c740ff025ffc3cad8bea0e545
luis barroso <luis.barroso@ifood.com.br>
2019-06-17 20:55:56 -0300

    FIX - Adding dependencies to package.json

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "./modules/Home";
 import React from "react";
-import { render } from "react-dom";
 
-render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact path={"/"} component={Home} />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+export default (App = () => {
+  (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={"/"} component={Home} />
+      </Switch>
+    </BrowserRouter>
+  ),
+    document.getElementById("root");
+});

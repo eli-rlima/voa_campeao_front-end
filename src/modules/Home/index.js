f5cee3e437c7179c67fdabe96995cc262b910d19
luis barroso <luis.barroso@ifood.com.br>
2019-06-17 21:08:53 -0300

    Adding navbar

--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -1,9 +1,11 @@
+import Navbar from "../Navbar";
 import React from "react";
 
 class Home extends React.Component {
   render() {
     return (
       <div>
+        <Navbar />
         <h1>Shopping List for DAle</h1>
         <ul>
           <li>Instagram</li>

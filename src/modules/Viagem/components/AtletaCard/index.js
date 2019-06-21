effa61800dbfa93ddd5e1a67e78eadeef5527cee
thiago costa <thiagocossta_@hotmail.com>
2019-06-21 18:36:41 -0300

    Fix| correcoes nos cards de Viagem

--- src/modules/Viagem/components/AtletaCard/index.js
+++ src/modules/Viagem/components/AtletaCard/index.js
@@ -4,14 +4,14 @@ import React from "react";
 import profile from "../../../../icons/profile.png";
 
 const AtletaCard = ({ viagem }) => (
-  <div className="container">
+  <div className="container_atleta">
     <div className="one">
       <img src={profile} alt="Avatar" className="image" />
     </div>
     <div classeName="two">
-      <h2> {viagem.atleta.nome}</h2>
-      <h2> 25 anos</h2>
-      <h2> {viagem.modalidade_comp}</h2>
+      <h2 className="detail_atleta"> {viagem.atleta.nome}</h2>
+      <h2 className="detail_atleta"> 25 anos</h2>
+      <h2 className="detail_atleta"> {viagem.modalidade_comp}</h2>
     </div>
   </div>
 );

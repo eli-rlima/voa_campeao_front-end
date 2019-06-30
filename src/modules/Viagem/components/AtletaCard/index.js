be7c17decca3dd4ce62b6f1754c9853eb01d3fb7
thiago costa <thiagocossta_@hotmail.com>
2019-06-30 19:53:15 -0300

    Feature| idade atleta e formatação de data

--- src/modules/Viagem/components/AtletaCard/index.js
+++ src/modules/Viagem/components/AtletaCard/index.js
@@ -1,6 +1,7 @@
 import "./index.css";
 
 import React from "react";
+import moment from "moment";
 import profile from "../../../../icons/profile.png";
 
 const AtletaCard = ({ viagem }) => (
@@ -10,7 +11,9 @@ const AtletaCard = ({ viagem }) => (
     </div>
     <div classeName="viagem__atleta">
       <h2 className="viagem__detalhe-atleta"> {viagem.atleta.nome}</h2>
-      <h2 className="viagem__detalhe-atleta"> 25 anos</h2>
+      <h2 className="viagem__detalhe-atleta">
+        {moment().diff(viagem.atleta.data_nascimento, "years")} anos
+      </h2>
       <h2 className="viagem__detalhe-atleta"> {viagem.modalidade_comp}</h2>
     </div>
   </div>
--- src/modules/Viagem/components/InfoCompCard/index.js
+++ src/modules/Viagem/components/InfoCompCard/index.js
@@ -1,15 +1,22 @@
 import "./index.css";
 
 import React from "react";
+import moment from "moment";
 
 const InfoCompCard = ({ viagem }) => (
   <div className="viagem__container-info">
     <div classeName="viagem__info">
       A competição de{" "}
       <span className="text_color">{viagem.modalidade_comp}</span> ocorrerá
-      entre os dias <span className="text_color">{viagem.data_ida}</span> e{" "}
-      <span className="text_color">{viagem.data_volta}</span>, na cidade de{" "}
-      <span className="text_color">{viagem.destino}</span>.
+      entre os dias{" "}
+      <span className="text_color">
+        {moment(viagem.data_ida).format("DD/MM/YYYY")}
+      </span>{" "}
+      e{" "}
+      <span className="text_color">
+        {moment(viagem.data_volta).format("DD/MM/YYYY")}
+      </span>
+      , na cidade de <span className="text_color">{viagem.destino}</span>.
     </div>
   </div>
 );
